b7adeaac674e52a4994b1823ef21bac23657743e
roniy68 <ahmed.11900449@lpu.in>
2023-06-03 14:32:55 +0530

    🔥 Add Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-end
+    before_action :configure_params, if: :devise_controller?
+  
+    protected
+  
+    def configure_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password confirm_password])
+    end
+  end
+  
